9a49f48b395ba13a6dcdb8bd84a8347f6b9cf353
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-11-13 22:58:42 +0600

    service details

---
+++ src/AllService/AllService.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+import { Link } from "react-router-dom";
+
+const AllService = ({ FoodService }) => {
+  const { title, img, price, _id, description } = FoodService;
+
+  return (
+    <div>
+      <div className="card  mb-4 bg-base-100 shadow-slate-300 shadow-2xl">
+        <figure>
+          <PhotoProvider>
+            <PhotoView src={img}>
+              <img src={img} alt="" />
+            </PhotoView>
+          </PhotoProvider>
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>Price:$ {price}</p>
+          <p>{description && description.slice(0, 100) + "..."}</p>
+          <div className="card-actions justify-end">
+            <Link to={`/serviceDetails/${_id}`}>
+              {" "}
+              <button className="btn btn-primary">Details</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllService;
